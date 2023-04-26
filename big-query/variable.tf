1d53af9a634af54e35e5ff14d6bee4c424094752
Charykarthik <128477106+Charykarthik@users.noreply.github.com>
2023-04-26 23:24:52 +0530

    Update variable.tf

--- big-query/variable.tf
+++ big-query/variable.tf
@@ -1,17 +1,21 @@
 
 variable "dataset_id" {
   type = string
+   default     = "karthik"
   description = "The ID of the BigQuery dataset to be created."
 }
 variable "default_partition_expiration_ms" {
   type = string
+   default     = "2900"
   
 }
 variable "location" {
   type        = string
+   default     = "us-central1"
 }
 variable "table_id" {
   type        = string
+   default     = "karthi"
   description = "The ID of the first BigQuery table to be created."
 }
 
@@ -19,16 +23,21 @@ variable "table_id" {
 
 variable "display_name" {
   type = string
+   default     = "karthik"
 }
 variable "region" {
   type = string
+   default     = "us-central1"
 }
 variable "data_source_id" {
   type = string
+   default     = "karthik"
 }
 variable "schedule" {
   type = string
+   default     = "2 * * * *"
 }
 variable "params" {
   type = string
+   default     = "cloud"
 }
