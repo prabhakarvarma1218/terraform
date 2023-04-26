34ad680f50db1bc0399f40d54fccb8f609d26c2e
Charykarthik <128477106+Charykarthik@users.noreply.github.com>
2023-04-26 15:51:37 +0530

    Add files via upload

---
+++ big-query/main.tf
@@ -0,0 +1,49 @@
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = var.dataset_id
+  description                 = "This is a test description"
+  location                    = var.location
+  default_table_expiration_ms = 3600000
+
+  labels = {
+    env = "default"
+  }
+}
+resource "google_bigquery_table" "table" {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  table_id   = var.table_id
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+  schema = <<EOF
+[
+  {
+    "name": "permalink",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "The Permalink"
+  },
+  {
+    "name": "state",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "State where the head office is located"
+  }
+]
+EOF
+
+}
+
+resource "google_bigquery_data_transfer_config" "query_config" {
+ 
+  display_name           = var.display_name
+  location               = var.region
+  data_source_id         = var.data_source_id
+  schedule               = var.schedule
+  destination_dataset_id = google_bigquery_dataset.dataset.dataset_id
+  params = var.params
+}
---
+++ big-query/variable.tf
@@ -0,0 +1,34 @@
+
+variable "dataset_id" {
+  type = string
+  description = "The ID of the BigQuery dataset to be created."
+}
+variable "default_partition_expiration_ms" {
+  type = string
+  
+}
+variable "location" {
+  type        = string
+}
+variable "table_id" {
+  type        = string
+  description = "The ID of the first BigQuery table to be created."
+}
+
+
+
+variable "display_name" {
+  type = string
+}
+variable "${var.region}" {
+  type = string
+}
+variable "data_source_id" {
+  type        = string
+}
+variable "schedule" {
+  type        = string
+}
+variable "params" {
+  type        = string
+}
