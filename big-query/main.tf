678b2d8b80f38812bf43458edd2f0281340fc5a2
Charykarthik <128477106+Charykarthik@users.noreply.github.com>
2023-04-27 15:11:20 +0530

    Update main.tf

--- big-query/main.tf
+++ big-query/main.tf
@@ -41,16 +41,3 @@ EOF
 
 }
 
-resource "google_bigquery_data_transfer_config" "query_config" {
- 
-  project                     = "bamboo-medium-381213"
-  display_name           = var.display_name
-  location               = var.region
-  data_source_id         = var.data_source_id
-  schedule               = var.schedule
-  destination_dataset_id = google_bigquery_dataset.dataset.dataset_id
-  params = {
-    destination_table_name_template = var.table_id
-    write_disposition               = "WRITE_APPEND"
-  }
-}
