56f5217972e06f00739be4b111ce1b723e691744
Charykarthik <128477106+Charykarthik@users.noreply.github.com>
2023-04-27 15:48:24 +0530

    Update and rename variable.tf.txt to variable.tf

--- GCS/variable.tf
+++ GCS/variable.tf
@@ -11,13 +11,12 @@ variable "project_id" {
 variable "gcs_location" {
   description = "GCS Location."
   type        = string
-  default     = "EU"
+  
 }
 
 variable "force_destroy" {
   description = "Delete all objects when deleting bucket."
   type        = bool
-  default     = false
 }
 
 variable "storage_class" {
@@ -26,7 +25,6 @@ The Storage class of the GCS bucket.
 Supported Values - STANDARD, MULTI_REGIONAL, REGIONAL, NEARLINE, COLDLINE.
 EOF
   type        = string
-  default     = "STANDARD"
 }
 
 variable "labels" {
@@ -37,5 +35,4 @@ variable "labels" {
 variable "enable_versioning" {
   description = "Enable GCS Bucket versioning."
   type        = bool
-  default     = false
 }
