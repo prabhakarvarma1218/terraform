5ffc49b11d820b2aa189bea9986e1a9c67d810d0
Charykarthik <128477106+Charykarthik@users.noreply.github.com>
2023-05-02 20:39:49 +0530

    Update main.tf

--- data-transfer/main.tf
+++ data-transfer/main.tf
@@ -1,4 +1,4 @@
-resource "google_bigquery_data_transfer_config" "gcs_to_bigquery" {
+resource "google_bigquery_data_transfer_config" "query_config" {
     display_name   = var.display_name
     data_source_id = var.data_source_id
     target_dataset = var.target_dataset
