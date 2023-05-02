14a01cb95a4942b303148397a2900fe9ede8f24c
Charykarthik <128477106+Charykarthik@users.noreply.github.com>
2023-05-02 21:52:46 +0530

    Update main.tf

--- data-transfer/main.tf
+++ data-transfer/main.tf
@@ -1,8 +1,8 @@
 resource "google_bigquery_data_transfer_config" "query_config" {
     display_name   = var.display_name
     data_source_id = var.data_source_id
-    target_dataset = var.target_dataset
-    target_table   = var.target_table
+    target_dataset = var.dataset_id
+    target_table   = var.table_id
     schedule       = var.schedule
     params = {
         source_objects = ["var.source_objects"]
