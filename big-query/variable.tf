e1805dc31acd48d58c656ff38c84b09ecf0e7261
Charykarthik <128477106+Charykarthik@users.noreply.github.com>
2023-05-03 11:25:48 +0530

    Update variable.tf

--- big-query/variable.tf
+++ big-query/variable.tf
@@ -1,29 +1,19 @@
-variable "project_id" {
-  type = string
-  default = "	bamboo-medium-381213"
-}
 variable "dataset_id" {
   type = string
-   default     = "karthik"
+  default = karthik
   description = "The ID of the BigQuery dataset to be created."
 }
 variable "default_partition_expiration_ms" {
-  type = string
-   default     = "2900"
+  type = number
+  default = 25000
   
 }
 variable "location" {
   type        = string
-   default     = "us-central1"
+  default = us-central1
 }
 variable "table_id" {
   type        = string
-   default     = "karthi"
+  default = karthi
   description = "The ID of the first BigQuery table to be created."
 }
-
-
-
-
-
-
