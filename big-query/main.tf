434d51e5407772a0262d36c5b230f0378ff3343a
Charykarthik <128477106+Charykarthik@users.noreply.github.com>
2023-05-03 10:37:14 +0530

    Update main.tf

--- big-query/main.tf
+++ big-query/main.tf
@@ -1,5 +1,5 @@
 resource "google_bigquery_dataset" "dataset" {
-  project                     = var.project
+  project                     = var.project_id
   dataset_id                  = var.dataset_id
   description                 = "This is a test description"
   location                    = var.location
