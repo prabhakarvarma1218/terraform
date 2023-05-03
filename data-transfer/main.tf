2124e203c0761d45029ede48949e94f129a8ede0
Charykarthik <128477106+Charykarthik@users.noreply.github.com>
2023-05-03 12:04:13 +0530

    Update main.tf

--- data-transfer/main.tf
+++ data-transfer/main.tf
@@ -2,7 +2,6 @@ resource "google_bigquery_data_transfer_config" "gcs_to_bigquery" {
     display_name   = var.display_name
     data_source_id = var.data_source_id
     destination_dataset_id = var.dataset_name
-    destination_table_id   = var.table_name
     schedule       = var.schedule
     params = {
         "data_path"  ="gs://${var.bucket_name}/*"
